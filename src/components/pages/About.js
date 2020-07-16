79b3042f7482a6092d920dea28fff03d651b7113
agentdonoso <agentdonoso@gmail.com>
2020-07-16 12:03:42 -0400

    Initial Commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is the TodoList app v1.0.0. Part of a React Crash Course. :p</p>
+    </React.Fragment>
+  );
+}
